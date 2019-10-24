f891fdd6605a9437c1d34dc86451cbe5d3db3fe4
glushkov <glushkov@proitr.ru>
2019-10-24 18:38:52 +0500

    sweater 10/50

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,40 @@
+package com.example.sweater.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+public class Message {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private String text;
+    private String tag;
+    private String filename;
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public Message() {
+    }
+
+    public Message(String text, String tag, User author) {
+        this.text = text;
+        this.tag = tag;
+        this.author = author;
+    }
+}
